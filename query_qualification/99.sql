3e1d5cee986ee7cb168ec97b9ca67972f164d8f7
mweisgut <marcel-weisgut@web.de>
2019-06-12 16:06:44 +0200

    changed query 99 for hyrise support

--- query_qualification/99.sql
+++ query_qualification/99.sql
@@ -1,6 +1,6 @@
 SELECT w_substr ,
        sm_type ,
-       LOWER(cc_name) cc_name_lower ,
+       cc_name ,
        sum(CASE
                WHEN (cs_ship_date_sk - cs_sold_date_sk <= 30) THEN 1
                ELSE 0
@@ -40,6 +40,6 @@ GROUP BY w_substr ,
          cc_name
 ORDER BY w_substr ,
          sm_type ,
-        cc_name_lower
+        cc_name
 LIMIT 100;
 
